8770f85087779eb2237693903a7776d377bbe6ad
Josué Jr <jsmilenium2003@hotmail.com>
2023-08-17 22:10:58 -0300

    Add custom exception for DocumentTypeController.

--- app/Http/Controllers/Api/DocumentTypeController.php
+++ app/Http/Controllers/Api/DocumentTypeController.php
@@ -6,6 +6,8 @@ use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Models\DocumentType;
 use PDF;
+use App\Exceptions\DocumentTypeException;
+
 
 class DocumentTypeController extends Controller
 {
@@ -16,82 +18,118 @@ class DocumentTypeController extends Controller
 
     public function index()
     {
-        $documentTypes = DocumentType::all();
+        try {
+            $documentTypes = DocumentType::all();
 
-        return response()->json($documentTypes);
+            return response()->json($documentTypes);
+        }catch(DocumentTypeException $e){
+            return response()->json([
+                'message' => $e->getMessage()
+            ], 404);
+        }
     }
 
     public function show($id, DocumentType $documentType)
     {
-        $document = DocumentType::find($id);
-        return response()->json($document);
+        try {
+            $document = DocumentType::find($id);
+            return response()->json($document);
+        }catch(DocumentTypeException $e){
+            return response()->json([
+                'message' => $e->getMessage()
+            ], 404);
+        }
     }
 
     public function store(Request $request)
     {
-        $data = $request->validate([
-            'name' => 'required|string',
-            'description' => 'required|string',
-            'document_type' => 'required|string',
-            'columns_and_fields' => 'required|array',
-        ]);
+        try {
+            $data = $request->validate([
+                'name' => 'required|string',
+                'description' => 'required|string',
+                'document_type' => 'required|string',
+                'columns_and_fields' => 'required|array',
+            ]);
 
-        $data['version'] = '1';
-        $data['columns_and_fields'] = json_encode($data['columns_and_fields']);
+            $data['version'] = '1';
+            $data['columns_and_fields'] = json_encode($data['columns_and_fields']);
 
-        $documentType = DocumentType::create($data);
+            $documentType = DocumentType::create($data);
 
-        return response()->json($documentType, 201);
+            return response()->json($documentType, 201);
+        }catch(DocumentTypeException $e){
+            return response()->json([
+                'message' => $e->getMessage()
+            ], 404);
+        }
     }
 
     public function update(Request $request, $id, DocumentType $documentType)
     {
-        $request->validate([
-            'name' => 'required|string',
-            'description' => 'required|string',
-            'document_type' => 'required|string',
-            'columns_and_fields' => 'required|array',
-        ]);
-        $document = DocumentType::find($id);
-        if(!$document){
+        try {
+            $request->validate([
+                'name' => 'required|string',
+                'description' => 'required|string',
+                'document_type' => 'required|string',
+                'columns_and_fields' => 'required|array',
+            ]);
+            $document = DocumentType::find($id);
+            if(!$document){
+                return response()->json([
+                    'message' => 'Document Type not found'
+                ], 404);
+            }
+            $data = [
+                'name' => $request->name,
+                'description' => $request->description,
+                'document_type' => $request->document_type,
+                'columns_and_fields' =>  json_encode($request->columns_and_fields),
+            ];
+
+            $document->update($data);
+
+            return response()->json($document);
+        }catch(DocumentTypeException $e){
             return response()->json([
-                'message' => 'Document Type not found'
+                'message' => $e->getMessage()
             ], 404);
         }
-        $data = [
-            'name' => $request->name,
-            'description' => $request->description,
-            'document_type' => $request->document_type,
-            'columns_and_fields' =>  json_encode($request->columns_and_fields),
-        ];
-
-        $document->update($data);
-
-        return response()->json($document);
     }
 
     public function destroy(DocumentType $documentType)
     {
-        $documentType->delete();
+        try {
+            $documentType->delete();
 
-        return response()->json(null, 204);
+            return response()->json(null, 204);
+        }catch(DocumentTypeException $e){
+            return response()->json([
+                'message' => $e->getMessage()
+            ], 404);
+        }
     }
 
     public function downloadPdf($id)
     {
-        $document = DocumentType::find($id);
-        if(!$document){
+        try {
+            $document = DocumentType::find($id);
+            if(!$document){
+                return response()->json([
+                    'message' => 'Document Type not found'
+                ], 404);
+            }
+            $data['name'] = $document->name;
+            $data['description'] = $document->description;
+            $data['columns_and_fields'] = $document->columns_and_fields;
+
+            $pdf = PDF::loadView('document_template', ['data' => $data]);
+
+            return $pdf->stream('document_' . Date('YmdHis') . '.pdf');
+        }catch(DocumentTypeException $e){
             return response()->json([
-                'message' => 'Document Type not found'
+                'message' => $e->getMessage()
             ], 404);
         }
-        $data['name'] = $document->name;
-        $data['description'] = $document->description;
-        $data['columns_and_fields'] = $document->columns_and_fields;
-
-        $pdf = PDF::loadView('document_template', ['data' => $data]);
-
-        return $pdf->stream('document_' . Date('YmdHis') . '.pdf');
     }
 
 }
